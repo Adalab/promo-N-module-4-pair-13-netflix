e6f4916eefd9f8518610980f757ec5da8f854d19
Sandra Fernandz <sandraenmunich@gmail.com>
2021-10-14 11:15:17 +0200

    solving exercise with template engine

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
 // create and config server
 const server = express();
@@ -12,27 +12,38 @@ server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
+// const foundMovie = movies.find((movie) => {
+//   if (movie.id === req.params.movieId) {
+//     console.log(foundMovie);
+//   }
+// });
+
+// este endpoint debe ir antes de los servidores estáticos sino no gestionará la petición pq la gestionarán los servidores estáticos
+server.get('/movie/:movieId', (req, res) => {
+  console.log(req.params.movieId);
+});
+
 // configuración del servidor de estáticos:
-const staticServerPath = "./src/public-react";
+const staticServerPath = './src/public-react';
 server.use(express.static(staticServerPath));
 
 // escribimos los endepoints que queramos
 
-server.get("/movies", (req, res) => {
+server.get('/movies', (req, res) => {
   const response = {
     success: true,
     movies: [
       {
-        id: "1",
-        title: "Gambita de dama",
-        gender: "Drama",
-        image: "https://via.placeholder.com/150",
+        id: '1',
+        title: 'Gambita de dama',
+        gender: 'Drama',
+        image: 'https://via.placeholder.com/150',
       },
       {
-        id: "2",
-        title: "Friends",
-        gender: "Comedia",
-        image: "https://via.placeholder.com/150",
+        id: '2',
+        title: 'Friends',
+        gender: 'Comedia',
+        image: 'https://via.placeholder.com/150',
       },
     ],
   };
