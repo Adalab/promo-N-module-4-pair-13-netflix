5937d3047113d5ebe4fd577220001f50ce54b289
Sandra Fernandz <sandraenmunich@gmail.com>
2021-10-18 11:27:07 +0200

    add checked sql query

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const cors = require("cors");
-const Database = require("better-sqlite3");
+const express = require('express');
+const cors = require('cors');
+const Database = require('better-sqlite3');
 
 // create and config server
 const server = express();
@@ -20,31 +20,35 @@ server.listen(serverPort, () => {
 // });
 
 // configurar db: esta constante apunta a una conexión a la base de datos
-const db = new Database("./src/db/database.db");
+const db = new Database('./src/db/database.db');
 
 //nuevo endpoint para registrar nuevas usuarias
-server.post("/login/:sign-up", (req, res) => {
+server.post('/login/:sign-up', (req, res) => {
   const email = req.body.email;
   const pass = req.body.pass;
-  const query = db.prepare("INSERT into users(email, pass) values(?, ?);");
+  const query = db.prepare('INSERT into users(email, pass) values(?, ?);');
   const userInsert = query.run(email, pass);
+  res.json({ userId: userInsert.lastInsertRowid });
+
   console.log();
 });
 
+//preg 19.10 verificar punto 1 si está correcto y app
+
 // este endpoint debe ir antes de los servidores estáticos sino no gestionará la petición pq la gestionarán los servidores estáticos
-server.get("/movie/:movieId", (req, res) => {
+server.get('/movie/:movieId', (req, res) => {
   console.log(req.params.movieId);
 });
 
 // configuración del servidor de estáticos:
-const staticServerPath = "./src/public-react";
+const staticServerPath = './src/public-react';
 server.use(express.static(staticServerPath));
 
 // escribimos los endepoints que queramos
 
-server.get("/movies", (req, res) => {
+server.get('/movies', (req, res) => {
   //Esta operación prepara una consulta a nuestra base de datos que selecciona todas las peliculas.
-  const moviesQuery = db.prepare("SELECT * FROM movies");
+  const moviesQuery = db.prepare('SELECT * FROM movies');
   // Aquí se ejecuta la consulta de arriba y pide que todas las peliculas seleccionadas sean devueltas como array de objetos
   const movies = moviesQuery.all();
   //console.log(movies);
